72aa16471c4c953d90d460ea0b8e8d593ac2c9e5
Maisha Yadav <21MYadav@heckgrammar.co.uk>
2024-10-23 14:11:24 +0100

    prefix

--- StringArrays10B24/Program.cs
+++ StringArrays10B24/Program.cs
@@ -1,29 +1,40 @@
-﻿namespace StringArrays10B24
+﻿namespace StringArrays10B2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            /* Write a subroutine called Prefix from your pseudo-code 
-             * The subroutine takes two, character arrays called word and pre as parameters and 
-             * The subroutine determines whether the start of the character array word is the 
-             * same as all of the character array pre.*/
+            {
+                string[] word1 = { "h", "e", "l", "l", "o" };
+                string[] pre1 = { "h", "e" };
+                string[] word2 = { "w", "o", "r", "l", "d" };
+                string[] pre2 = { "w", "o", "r", "l", "d" };
+                string[] word3 = { "t", "e", "s", "t" };
+                string[] pre3 = { "t", "e", "s", "t", "i", "n", "g" };
+                Console.WriteLine(Prefix(word1, pre1));
+                Console.WriteLine(Prefix(word2, pre2));
+                Console.WriteLine(Prefix(word3, pre3));
+            }
+            static bool Prefix(string[] word, string[] pre)
+            {
+                if (pre.Length > word.Length)
+                {
+                   return false;
+                }
+                for (int i = 0; i < pre.Length; i++)
+                {
+                    if (word[i] != pre[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
 
-            /* Your subroutine should also:
-	•           work for character arrays of all lengths greater than 0
-	•           not assume that the length of pre is less than the length of word.*/
-
-            /* use fixed (hard coded) string arrays to test your code. 
-             Call your subroutine using your test arrays from Main() routine
-             and return the result to Main() */
-            
-            /* Starter code for your subroutine has been provided*/
+            }
 
         }
-        static bool Prefix(string[] wor, string[] pre) 
-        {
 
-            return;
-        }
     }
 }
+
+
